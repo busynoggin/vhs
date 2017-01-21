10b11789dcf65d56a8980e2cbdf9ece2ff7e8060
Jeff Segars <jeff@busynoggin.com>
2017-01-21 16:05:51 -0600

    Revert "[REMOVAL] Remove PHP5 support (#1204)"
    This reverts commit b4fc2f45d845a16e3a237d6ac0a0c5539842ee41.

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'php' => '7.0.0-7.1.99',
+      'php' => '5.5.0-7.0.99',
       'typo3' => '7.6.13-8.5.99',
     ),
     'conflicts' =>
